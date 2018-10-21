06a87b20d7bc651a5f00ca60b41be2cf4feccedc
Alexander Proschenko <aproschenko.dev@gmail.com>
2018-10-21 17:46:31 +0300

    add blynk code

---
+++ blynk/config.js
@@ -0,0 +1,9 @@
+var config = {
+    blynkUrl: "https://192.168.1.212:9443/b2d5d60492ec4348847dea2c0e4d30fa",
+    serverPort: 9898,
+    multicastAddress: '224.0.0.50',
+    multicastPort: 4321,
+    sensorDelay: 30
+};
+
+module.exports = config;
